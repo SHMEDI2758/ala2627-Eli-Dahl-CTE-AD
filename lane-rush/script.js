aa3b2faab5459d000d0fe970893ae0a8911e836e
SHMEDI2758 <ed71581@stu.alaschools.org>
2026-09-25 21:27:45 +0000

    do you come from a landdown under

--- lane-rush/script.js
+++ lane-rush/script.js
@@ -139,7 +139,7 @@ function update(delta) {
   }
 
   const obstacleSpeed = road.height * (0.34 + game.speed * 0.075);
-  game.roadOffset += delta * obstacleSpeed;
+  game.roadOffset += delta * obstacleSpeed * 0.5;
   game.obstacles.forEach((obstacle) => { obstacle.y += delta * obstacleSpeed; });
   game.obstacles = game.obstacles.filter((obstacle) => obstacle.y < road.height + 130);
 
